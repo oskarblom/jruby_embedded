a269ccbd5e0138319bc147a82fa2ac3d664b9334
oskarblom <oskar.blom@gmail.com>
2010-03-30 16:20:56 +0200

    added classpath

--- src/org/thunderflower/hello/Driver.java
+++ src/org/thunderflower/hello/Driver.java
@@ -1,5 +1,6 @@
 package org.thunderflower.hello;
 
+import org.jruby.embed.PathType;
 import org.jruby.embed.ScriptingContainer;
 
 public class Driver {
@@ -7,8 +8,8 @@ public class Driver {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		new ScriptingContainer().runScriptlet("puts 'Hello, jruby!'");
+	public static void main(String[] args) throws InterruptedException {
+		ScriptingContainer container = new ScriptingContainer();		
+		container.runScriptlet(PathType.CLASSPATH, "hello.rb");
 	}
-
 }
